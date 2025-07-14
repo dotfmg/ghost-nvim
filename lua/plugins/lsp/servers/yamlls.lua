0ded7236e4301b8d284ac6beb2a0648905c69433
Franklin Garcia <fm-garcia@outlook.com>
2025-07-14 16:58:48 -0500

    feat(lsp): add Helm and Bash language server support
    - Configured `helm_ls` for Helm templates and values files.
    - Configured `bashls` for shell scripting with exclusions for `.env`
    files.
    - Updated YAML language server settings to support Helm templates.

--- lua/plugins/lsp/servers/yamlls.lua
+++ lua/plugins/lsp/servers/yamlls.lua
@@ -1,28 +1,27 @@
 -- Configures yamlls (YAML Language Server) for YAML LSP features.
 
-local lspconfig = require('lspconfig')
+local lspconfig = require 'lspconfig'
 
 lspconfig.yamlls.setup {
   settings = {
-    yaml = { -- Settings for yamlls.
-      validate = true, -- Enable validation against schemas/syntax.
-      hover = true,    -- Enable hover information.
-      completion = true, -- Enable completion suggestions.
-
-      -- Map JSON Schema URIs to YAML file glob patterns for validation.
+    yaml = {
+      validate = true,
+      hover = true,
+      completion = true,
+      format = {
+        enable = true,
+        singleQuote = true,
+      },
+      schemaStore = {
+        enable = true,
+      },
       schemas = {
         ['https://json.schemastore.org/github-workflow.json'] = '/.github/workflows/*',
-        ['https://json.schemastore.org/kustomization.json'] = 'kustomization.yaml',
-        ['https://json.schemastore.org/kubernetes.json'] = '/*.k8s.yaml',
-        ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = 'docker-compose*.yaml',
+        ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = '*compose*.yaml',
+        ['https://www.schemastore.org/chart.json'] = 'Chart.yaml',
+        ['https://www.schemastore.org/helmfile.json'] = 'helmfile*.yaml',
       },
-
-      format = {
-        enable = true, -- Enable yamlls's built-in formatter.
-                       -- Disable if using Prettier via none-ls for YAML.
-      },
-      -- customTags = { "!Ref", "!GetAtt" }, -- Example for custom YAML tags.
+      customTags = { '!Ref', '!GetAtt', '!Sub', '!If' },
     },
   },
-  -- on_attach and capabilities are inherited from global LSP setup.
 }
